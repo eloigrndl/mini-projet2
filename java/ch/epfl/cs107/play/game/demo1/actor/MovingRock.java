10cd380e0edcde1e39e7d516fe2cb6a45e37a870
Antoine Bellanger <antoinebelldev@gmail.com>
2018-11-23 14:00:38 +0100

    //Rock

--- java/ch/epfl/cs107/play/game/demo1/actor/MovingRock.java
+++ java/ch/epfl/cs107/play/game/demo1/actor/MovingRock.java
@@ -13,7 +13,7 @@ public class MovingRock extends GraphicsEntity {
     private final TextGraphics text;
 
     public MovingRock(Vector position, String text) {
-
+        //Rock
         super(position,new ImageGraphics(ResourcePath.getSprite("rock.3"), 0.1f,0.1f, null, Vector.ZERO, 1.0f, -Float.MAX_VALUE));
 
         this.text = new TextGraphics(text, 0.04f, Color.BLUE);
